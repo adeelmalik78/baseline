ef353770e645e09f57c36a8c9ac9198dae15cea3
git <adeelmalik78@gmail.com>
2026-03-16 16:01:40 -0500

    baseline.ps1

--- baseline.ps1
+++ baseline.ps1
@@ -1,4 +1,5 @@
 $env:BASELINE_CHANGELOG_FILE = "baseline.mssql.sql"
+$env:BASELINE_DATA_CHANGELOG_FILE = "baseline_data.mssql.sql"
 
 if (Test-Path $env:BASELINE_CHANGELOG_FILE) {
     Remove-Item $env:BASELINE_CHANGELOG_FILE -Force
@@ -8,8 +9,21 @@ if (Test-Path "baseline" -PathType Container) {
     Remove-Item "baseline" -Recurse -Force
 }
 
-liquibase --changelog-file=$env:BASELINE_CHANGELOG_FILE generate-changelog 
-
+# Generate baseline for objects
+liquibase --changelog-file=$env:BASELINE_CHANGELOG_FILE generate-changelog `
+    --exclude-objects="schema:sys,schema:information_schema,table:DATABASECHANGELOG,table:DATABASECHANGELOGLOCK"
 Get-Item $env:BASELINE_CHANGELOG_FILE | Select-Object Name, Length, LastWriteTime
-
 python split_sql.py $env:BASELINE_CHANGELOG_FILE
+
+# Generate data for specific tables (only if table arguments are provided)
+if ($args.Count -gt 0) {
+    liquibase --changelog-file=$env:BASELINE_DATA_CHANGELOG_FILE generate-changelog `
+        --diff-types="data" --include-objects="$($args -join ',')"
+    Get-Item $env:BASELINE_DATA_CHANGELOG_FILE | Select-Object Name, Length, LastWriteTime
+    python split_sql.py $env:BASELINE_DATA_CHANGELOG_FILE
+} else {
+    Write-Host "No data was requested. If you need data, run ..."
+    Write-Host " .\baseline.ps1 table:<table1>,table:<table2>,..."
+    Write-Host "e.g., "
+    Write-Host ".\baseline.ps1 table:ProductCategory,table:Address"
+}
