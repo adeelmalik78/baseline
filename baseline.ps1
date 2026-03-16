bc4904896defcd408ef9a67833fceef76b3babab
git <adeelmalik78@gmail.com>
2026-03-16 12:33:11 -0500

    fixed powershell script

--- baseline.ps1
+++ baseline.ps1
@@ -1,15 +1,15 @@
-$BASELINE_CHANGELOG_FILE = "baseline.mssql.sql"
+$env:BASELINE_CHANGELOG_FILE = "baseline.mssql.sql"
 
-if (Test-Path $BASELINE_CHANGELOG_FILE) {
-    Remove-Item $BASELINE_CHANGELOG_FILE -Force
+if (Test-Path $env:BASELINE_CHANGELOG_FILE) {
+    Remove-Item $env:BASELINE_CHANGELOG_FILE -Force
 }
 
 if (Test-Path "baseline\sqls" -PathType Container) {
     Remove-Item "baseline\sqls" -Recurse -Force
 }
 
-liquibase --changelog-file=$BASELINE_CHANGELOG_FILE generate-changelog --diff-types="tables,columns,indexes,foreignkeys,primarykeys,uniqueconstraints,functions,views,storedprocedures,triggers,sequences,catalogs,schemas"
+liquibase --changelog-file=$env:BASELINE_CHANGELOG_FILE generate-changelog --diff-types="tables,columns,indexes,foreignkeys,primarykeys,uniqueconstraints,functions,views,storedprocedures,triggers,sequences,catalogs,schemas"
 
-Get-Item $BASELINE_CHANGELOG_FILE | Select-Object Name, Length, LastWriteTime
+Get-Item $env:BASELINE_CHANGELOG_FILE | Select-Object Name, Length, LastWriteTime
 
-python split_sql.py $BASELINE_CHANGELOG_FILE
+python split_sql.py $env:BASELINE_CHANGELOG_FILE
